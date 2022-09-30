fd069c9cd31cc0783075de0cd50cb2507e807bd4
Anna Klingauf <a.klingauf@micromata.de>
2022-09-30 19:41:12 +0200

    BE: Remove sample code, implement easy check for working correctly

--- backend/src/main/kotlin/de/heavy_feedback/Application.kt
+++ backend/src/main/kotlin/de/heavy_feedback/Application.kt
@@ -5,14 +5,23 @@ import de.heavy_feedback.plugins.configureRouting
 import de.heavy_feedback.plugins.configureSecurity
 import de.heavy_feedback.plugins.configureSerialization
 import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun main(args: Array<String>): Unit =
     io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
-    //configureSecurity()
-    //configureHTTP()
+    configureSecurity()
+    configureHTTP()
     configureSerialization()
     configureRouting()
+
+    routing {
+        get("/") {
+            call.respondText { "Yes it works!" }
+        }
+    }
+
 }
--- backend/src/main/kotlin/de/heavy_feedback/plugins/HTTP.kt
+++ backend/src/main/kotlin/de/heavy_feedback/plugins/HTTP.kt
@@ -3,10 +3,7 @@ package de.heavy_feedback.plugins
 import io.ktor.http.*
 import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.plugins.defaultheaders.*
-import io.ktor.server.plugins.httpsredirect.*
 import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
 
 fun Application.configureHTTP() {
     install(CORS) {
@@ -21,11 +18,12 @@ fun Application.configureHTTP() {
     install(DefaultHeaders) {
         header("X-Engine", "Ktor") // will send this header with each response
     }
-    install(HttpsRedirect) {
+    // Deactivate HTTPS redirect for now
+    /*install(HttpsRedirect) {
         // The port to redirect to. By default 443, the default HTTPS port.
         sslPort = 443
         // 301 Moved Permanently, or 302 Found redirect.
         permanentRedirect = true
-    }
+    }*/
 
 }
