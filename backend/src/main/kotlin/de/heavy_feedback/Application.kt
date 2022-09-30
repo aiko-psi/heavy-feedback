7483f7204913cce13bead66f11fa67faeb9de106
Anna Klingauf <a.klingauf@micromata.de>
2022-09-30 18:19:48 +0200

    BE: Switch to using a hocon file for ktor

--- backend/src/main/kotlin/de/heavy_feedback/Application.kt
+++ backend/src/main/kotlin/de/heavy_feedback/Application.kt
@@ -1,23 +1,18 @@
 package de.heavy_feedback
 
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
-import de.heavy_feedback.plugins.*
+import de.heavy_feedback.plugins.configureHTTP
+import de.heavy_feedback.plugins.configureRouting
+import de.heavy_feedback.plugins.configureSecurity
+import de.heavy_feedback.plugins.configureSerialization
 import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
 
-fun main() {
-    embeddedServer(Netty, port = 8080) {
-        // configureSecurity()
-        //configureHTTP()
-        configureSerialization()
-        configureRouting()
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
 
-        routing {
-            get("/") {
-                call.respondText { "Yes it works!" }
-            }
-        }
-    }.start(wait = true)
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    //configureSecurity()
+    //configureHTTP()
+    configureSerialization()
+    configureRouting()
 }
