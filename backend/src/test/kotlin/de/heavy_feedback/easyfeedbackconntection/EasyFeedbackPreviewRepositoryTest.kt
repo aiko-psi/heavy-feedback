1280b0d7dfa8e5ed5f55874420ed8ec34033d977
Anna Klingauf <a.klingauf@micromata.de>
2023-03-29 15:08:14 +0200

    BE: Add test for no internet

--- backend/src/test/kotlin/de/heavy_feedback/easyfeedbackconntection/EasyFeedbackPreviewRepositoryTest.kt
+++ backend/src/test/kotlin/de/heavy_feedback/easyfeedbackconntection/EasyFeedbackPreviewRepositoryTest.kt
@@ -2,8 +2,13 @@ package de.heavy_feedback.easyfeedbackconntection
 
 import de.heavy_feedback.AppModule
 import de.heavy_feedback.easyfeedbackconnection.EasyFeedbackPreviewRepository
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.apache.Apache
+import io.ktor.client.request.get
+import io.ktor.http.HttpStatusCode
 import kotlinx.coroutines.runBlocking
 import org.junit.After
+import org.junit.Assume
 import org.junit.Before
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
@@ -15,12 +20,26 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
+/**
+ * This class of tests needs an internet connection because it tests the retrieval of data from the real easyfeedback api.
+ * If there is no internet connection, most of the tests are skipped.
+ */
 class EasyFeedbackPreviewRepositoryTest : KoinTest {
 
     private val easyFeedbackPreviewRepository: EasyFeedbackPreviewRepository by inject()
 
     private var workingTestUrl: String? = null
     private var workingOriginalUrl: String? = null
+
+    private suspend fun testInternet(): Boolean {
+        val client = HttpClient(Apache)
+        return try {
+            val testCallResult = client.get("https://www.google.de/")
+            testCallResult.status == HttpStatusCode.OK
+        } catch (e: Throwable) {
+            false
+        }
+    }
 
     @Before
     fun initialiseTest() {
@@ -48,6 +67,9 @@ class EasyFeedbackPreviewRepositoryTest : KoinTest {
     fun getApiToken_validUrl_returnApiToken() {
         assertNotNull(workingOriginalUrl)
         runBlocking {
+            // Only run this test if there is internet
+            Assume.assumeTrue(testInternet())
+
             val result = easyFeedbackPreviewRepository.getApiToken(workingTestUrl!!)
             assertNotNull(result)
         }
@@ -57,7 +79,11 @@ class EasyFeedbackPreviewRepositoryTest : KoinTest {
     fun getSurveyInfo_validUrl_returnPagesWithQuestions() {
         assertNotNull(workingOriginalUrl)
         runBlocking {
+            // Only run this test if there is internet
+            Assume.assumeTrue(testInternet())
+
             val result = easyFeedbackPreviewRepository.getSurveyInfo(workingTestUrl!!)
+            // Test questionnaire has two pages at the moment
             assertEquals(8, result.pages.size)
         }
     }
