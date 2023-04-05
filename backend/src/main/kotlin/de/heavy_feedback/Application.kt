4ad85ab99c308b45f5b17bdbd2337c8eff795d1d
Anna Klingauf <a.klingauf@micromata.de>
2023-04-05 14:33:51 +0200

    BE: Refactor mapping from ktor to koin

--- backend/src/main/kotlin/de/heavy_feedback/Application.kt
+++ backend/src/main/kotlin/de/heavy_feedback/Application.kt
@@ -1,15 +1,18 @@
 package de.heavy_feedback
 
+import de.heavy_feedback.plugins.KoinController
 import de.heavy_feedback.plugins.configureHTTP
 import de.heavy_feedback.plugins.configureRouting
 import de.heavy_feedback.plugins.configureSecurity
 import de.heavy_feedback.plugins.configureSerialization
+import de.heavy_feedback.utlis.toMapNotNull
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
 import io.ktor.server.application.install
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
+import org.koin.java.KoinJavaComponent.getKoin
 import org.koin.ksp.generated.module
 import org.koin.ktor.plugin.Koin
 
@@ -23,15 +26,12 @@ fun Application.module() {
     configureSerialization()
     configureRouting()
 
-    // Use the ktor config and put it in koin to make it available to all components
-    val configMap: Map<String, Any> = environment.config.toMap()
-        .mapNotNull { (key, value) -> if (value != null) key to value else null }
-        .toMap()
-
     install(Koin) {
         // If the IDE does not like the module, try running gradle kspTestKotlin
         modules(AppModule().module)
-        properties(configMap)
+
+        // Use the ktor config and put it in koin to make it available to all components
+        properties(environment.config.toMapNotNull())
     }
 
     routing {
